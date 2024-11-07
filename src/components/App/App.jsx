b298463046b063b978f5983a65048f05539d86e3
Oleksandr <45110010+Oleksandr-Karnaushenko@users.noreply.github.com>
2024-11-07 22:28:34 +0100

    init commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      {/* <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">Catalog</NavLink>
+      </nav> */}
+
+      {/* <Routes>
+        <Route path="" element={} />
+        <Route path="" element={} />
+        <Route path="" element={} />
+        <Route path="*" element={<NotFound />} />
+      </Routes> */}
+    </>
+  );
+}
+
+export default App;
